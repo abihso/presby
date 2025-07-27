6269f327ba8129a36207b41ea3a8a8987ebda79f
abihso <solomonpwatio@gmail.com>
2025-07-26 20:05:03 -0700

    Worked on first route trial

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Ensure this matches your deployment path
-  server: {
-    historyApiFallback: true,
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  }
+  plugins: [react()]
 })
